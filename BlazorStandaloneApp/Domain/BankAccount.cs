6651f416aa213951943008331cf896183592bd49
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2025-10-10 11:09:54 +0200

    bl lagt till fungerande accounts

--- BlazorStandaloneApp/Domain/BankAccount.cs
+++ BlazorStandaloneApp/Domain/BankAccount.cs
@@ -4,6 +4,7 @@ namespace BlazorStandaloneApp.Domain;
 public class BankAccount : IBankAccount
 {
     public Guid Id { get; private set; } = Guid.NewGuid();
+    public AccountType AccountType { get; private set; }
 
     public string Name { get; private set; }
 
@@ -12,6 +13,15 @@ public class BankAccount : IBankAccount
     public decimal Balance { get; private set; }
 
     public DateTime LastUpdated { get; private set; }
+
+    public BankAccount(string name, AccountType accountType, string currency, decimal initialBalance)
+    {
+        Name = name;
+        AccountType = accountType;
+        Currency = currency;
+        Balance = initialBalance;
+        LastUpdated = DateTime.Now;
+    }
 
     public void Deposit(decimal amount)
     {
